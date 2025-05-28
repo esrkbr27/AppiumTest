d22bcca294cb6b1af2d121f673f3f43e96bfa521
esrkbr27 <esraakdagdemirci@gmail.com>
2025-05-28 17:04:26 +0300

    23.05.2025

--- src/test/java/Gestures.java
+++ src/test/java/Gestures.java
@@ -1,7 +1,18 @@
 import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Gestures {
     AppiumDriver driver=Driver.initilazeDriver();
@@ -15,6 +26,39 @@ public class Gestures {
         //        "velocity", 2500,
                 "direction", "down"));
         //        "elementId", e.getId() ));
+    }
+
+    public static void scrollGesture(AppiumDriver driver){
+
+        driver.executeScript("mobile: scroll", ImmutableMap.of(
+                "direction", "down"
+        ));
+    }
+
+
+    public static void tap(AppiumDriver driver){
+      //  RemoteWebElement e = (RemoteWebElement) driver.findElement(AppiumBy.accessibilityId("Picker View"));
+        driver.executeScript("mobile: tap", ImmutableMap.of(
+       //         "elementId", e.getId(),
+                "x", 19,
+                "y", 366
+        ));
+    }
+
+    public static void clickElement(AppiumDriver driver){
+        driver.findElement(AppiumBy.iOSNsPredicateString("name == \"Image View\"")).click();
+    }
+
+    public static void takeScreenShot(AppiumDriver driver) throws IOException {
+        String date= new SimpleDateFormat("yyyyMMhhdd").format(new Date());
+        TakesScreenshot ts= Driver.initilazeDriver();
+
+        File source= ts.getScreenshotAs(OutputType.FILE);
+
+        String target= System.getProperty("user.dir")+ "/target/Screenshots/"+ source.getName() + date + ".png";
+        File finaldestination= new File(target);
+        FileUtils.copyFile(source, finaldestination);
+
     }
 
 }
